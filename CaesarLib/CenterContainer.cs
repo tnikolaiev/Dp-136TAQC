bd0a987cacfc1e6099cef8da1b1cdf7abab33684
kalyanov04 <alex.kalyanov@users.noreply.github.com>
2018-07-16 10:51:01 +0300

    Added method 'IsHintVisible'
    Method 'IsHintVisible'  finds element 'hint' by XPath and check if it visible.

--- CaesarLib/CenterContainer.cs
+++ CaesarLib/CenterContainer.cs
@@ -1,9 +1,6 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CaesarLib
 {
@@ -17,6 +14,10 @@ namespace CaesarLib
         public CenterContainer(IWebDriver webDriver)
         {
             this.webDriver = webDriver;
+        }
+        public Func<IWebDriver, IWebElement> IsHintVisible()
+        {
+            return ExpectedConditions.ElementIsVisible(By.XPath("//*[@class='hint']/p[@class='hint']"));
         }
     }
 }
