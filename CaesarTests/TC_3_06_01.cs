81edd403efc9c3778e6c27aacea6f385b6c43e0f
kalyanov04 <alex.kalyanov@users.noreply.github.com>
2018-07-25 02:13:39 +0300

    Changed 'TC_3_06_01' 'TC_3_08'
    Added data driving to 'TC_3_08'

--- CaesarTests/TC_3_06_01.cs
+++ CaesarTests/TC_3_06_01.cs
@@ -14,48 +14,46 @@ namespace CaesarTests
         WebDriverWait wait;
         string baseURL = "localhost:3000";
         LoginPage loginPageInstance;
-        StudentsContent studentsContentInstance;
-        EditStudentListWindow editStudentListWindowInstance;
+        MainPage mainPageInstance;
         String path;
 
         [OneTimeSetUp]
         public void OneTimeSetUp()
         {
-            wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(5));
-
+            wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(1));
             webDriver.Manage().Window.Maximize();
-            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+            //Open Login Page
             webDriver.Url = baseURL;
-
+            wait.Until((driver) => LoginPage.IsLoginPageOpened(driver));
             loginPageInstance = new LoginPage(webDriver);
+            //Login as Teacher
             loginPageInstance.LogIn("sasha", "1234");
             wait.Until((d) => MainPage.IsMainPageOpened(d));
-
+            mainPageInstance = new MainPage(webDriver);
+            //Go to group's LV-023-UX students page
             webDriver.Url = baseURL + "/Students/Lviv/Lv-023-UX/list";
-
-            studentsContentInstance = new StudentsContent(webDriver);
             wait.Until((d) => StudentsContent.IsStudentsContentOpened(d));
-
-            Acts.Click(studentsContentInstance.EditButton);
-
-            editStudentListWindowInstance = new EditStudentListWindow(webDriver);
+            //Open modal window 'EditStudentListWindow'
+            mainPageInstance.CenterContainer.StudentsContent.EditButton.Click();
             wait.Until((d) => EditStudentListWindow.IsEditStudentListWindowOpened(d));
         }
         [Test]
-        public void ExecuteTest_ImportStudentList_ListImpoerted()
+        public void ExecuteTest_ImportStudentList_ListImported()
         {
-            Acts.Click(editStudentListWindowInstance.ImportStudentsButton);
+            mainPageInstance.ModalWindow.EditStudentListWindow.ImportStudentsButton.Click();
             path = EditStudentListWindow.GetTestFile("TC_3_06_01 ValidStudentList.txt");
             Acts.UploadFile(path);
-            Acts.Click(editStudentListWindowInstance.SaveFormButton);
-            Assert.AreEqual(4, editStudentListWindowInstance.Students.Count);
+            mainPageInstance.ModalWindow.EditStudentListWindow.SaveFormButton.Click();
+            Assert.AreEqual(4, mainPageInstance.ModalWindow.EditStudentListWindow.Students.Count);
         }
         [OneTimeTearDown]
         public void OneTimeTearDownTest()
         {
-            while (editStudentListWindowInstance.DeleteButtons.Count != 0)
+            while (mainPageInstance.ModalWindow.EditStudentListWindow.DeleteButtons.Count != 0)
             {
-                Acts.Click(editStudentListWindowInstance.GetLastElement(editStudentListWindowInstance.DeleteButtons));
+                mainPageInstance.ModalWindow.EditStudentListWindow.StudentTable.GetElementFromCell
+               (mainPageInstance.ModalWindow.EditStudentListWindow.Students.Count, EditStudentListWindow.DeleteButtonsColumn).Click();
                 Acts.PressKeyboardButton(@"{Enter}");
             }
             webDriver.Close();
